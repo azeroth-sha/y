0f316ccd610aeadf03b74e9460d3df638eeb9f17
YannianyYi <yiyannian@chivalrous-tech.com>
2025-07-21 17:48:57 +0800

    refactor(yconst): 明确常量类型以提高代码可读性和安全性
    - 为 IntSize 和 IntCap 常量指定 int 类型
    - 为 MaxFloat32 和 MaxFloat64 常量指定 float32 和 float64 类型
    - 这些修改确保了常量类型的明确性，减少了潜在的类型推断错误

--- yconst/number.go
+++ yconst/number.go
@@ -1,8 +1,8 @@
 package yconst
 
 const (
-	IntSize = 32 << (^uint(0) >> 63) // 32 or 64
-	IntCap  = IntSize / 8
+	IntSize int = 32 << (^uint(0) >> 63) // 32 or 64
+	IntCap  int = IntSize / 8
 
 	MaxInt   int   = 1<<(IntSize-1) - 1
 	MaxInt8  int8  = 1<<7 - 1
@@ -22,6 +22,6 @@ const (
 	MaxUint32 uint32 = 1<<32 - 1
 	MaxUint64 uint64 = 1<<64 - 1
 
-	MaxFloat32 = 0x1p127 * (1 + (1 - 0x1p-23))  // 3.40282346638528859811704183484516925440e+38
-	MaxFloat64 = 0x1p1023 * (1 + (1 - 0x1p-52)) // 1.79769313486231570814527423731704356798070e+308
+	MaxFloat32 float32 = 0x1p127 * (1 + (1 - 0x1p-23))  // 3.40282346638528859811704183484516925440e+38
+	MaxFloat64 float64 = 0x1p1023 * (1 + (1 - 0x1p-52)) // 1.79769313486231570814527423731704356798070e+308
 )
